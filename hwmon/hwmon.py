94acd875656a706348dc75b7958e6b5eb2fc56b1
lucas liu <lucli@celestica.com>
2024-06-13 10:04:05 +0800

    update code to adapt all platform

--- hwmon/hwmon.py
+++ hwmon/hwmon.py
@@ -14,9 +14,27 @@ class Hwmon():
         is_vm()
 
     class HW():
-
         def __init__(self):
             self.master_path = '/sys/class/hwmon'
+            self.attributes_list = ["_max", "_min", "_crit", "_lcrit"]
+
+        def value_format(self, attributes_file, value):
+
+             # See https://www.kernel.org/doc/Documentation/hwmon/sysfs-interface
+            if attributes_file.lower().startswith('in'):
+                return str(int(value) / 1000) + ' V'
+            elif attributes_file.lower().startswith('fan'):
+                return value + ' RPM'
+            elif attributes_file.lower().startswith('pwm'):
+                return str(int(value) / 255) + ' PWM (%)'
+            elif attributes_file.lower().startswith('temp'):
+                return str(int(value) / 1000) + ' C'
+            elif attributes_file.lower().startswith('curr'):
+                return str(int(value) / 1000) + ' A'
+            elif attributes_file.lower().startswith('power'):
+                return str(int(value) / 1000000) + ' W'
+            elif attributes_file.lower().startswith('freq'):
+                return str(int(value) / 1000000) + ' MHz'
 
         def read_data(self, data_path):
             file = open(data_path, 'r')
@@ -26,9 +44,16 @@ class Hwmon():
 
         def extract_data(self, sub_folder_path, file_):
 
-            if os.path.exists(os.path.join(sub_folder_path, file_.split('_')[0] + '_label')):
+            # initial hwmon data list
+            hwmon_data = []
+            data = dict()
+            # split file header
+            file_key = file_.split('_')[0]
 
-                label_name = file_.split('_')[0] + '_label'
+            # read in/fan/temp/curr/power/energy/humidity[0-*]_input data
+            if os.path.exists(os.path.join(sub_folder_path, file_key + '_label')):
+
+                label_name = file_key + '_label'
 
                 label_name = self.read_data(os.path.join(sub_folder_path, label_name))
                 # only read input data, not to read the "*_input_highest" and "*_input_lowest"
@@ -37,24 +62,21 @@ class Hwmon():
 
             else:
 
-                label_name = file_.split('_')[0]
+                label_name = file_key
                 value = self.read_data(os.path.join(sub_folder_path, file_))
 
-            # See https://www.kernel.org/doc/Documentation/hwmon/sysfs-interface
-            if file_.lower().startswith('in'):
-                return label_name, str(int(value) / 1000) + ' V'
-            elif file_.lower().startswith('fan'):
-                return label_name, value + ' RPM'
-            elif file_.lower().startswith('pwm'):
-                return label_name, str(int(value) / 255) + ' PWM (%)'
-            elif file_.lower().startswith('temp'):
-                return label_name, str(int(value) / 1000) + ' C'
-            elif file_.lower().startswith('curr'):
-                return label_name, str(int(value) / 1000) + ' A'
-            elif file_.lower().startswith('power'):
-                return label_name, str(int(value) / 1000000) + ' W'
-            elif file_.lower().startswith('freq'):
-                return label_name, str(int(value) / 1000000) + ' MHz'
+            data[label_name] = self.value_format(file_, value)
+            hwmon_data.append(data)
+
+            for file in self.attributes_list:
+                file_id = file_key + file
+                file_name = label_name + file
+                if os.path.exists(os.path.join(sub_folder_path, file_id)):
+                    value = self.read_data(os.path.join(sub_folder_path, file_id))
+                    data[file_name] = self.value_format(file_id, value)
+                    hwmon_data.append(data)
+
+            return hwmon_data
 
         def data(self):
 
@@ -64,11 +86,13 @@ class Hwmon():
 
             for folder in folders:
 
+                name_key = None
                 sub_folder_path = os.path.join(self.master_path, folder)
 
                 files = os.listdir(sub_folder_path)
 
-                name_key = self.read_data(os.path.join(sub_folder_path, 'name'))
+                if os.path.exists(os.path.join(sub_folder_path, 'name')):
+                    name_key = self.read_data(os.path.join(sub_folder_path, 'name'))
 
                 symlink = os.readlink(os.path.join(sub_folder_path, 'device'))
                 symlink = symlink.strip().split("/")[-1]
@@ -81,12 +105,14 @@ class Hwmon():
                     try:
 
                         if '_input' in file_:
-                            label_name, value = self.extract_data(sub_folder_path, file_)
-                            data[sensor_name][label_name] = value
+                            hwmon_data = self.extract_data(sub_folder_path, file_)
+                            for label_name in hwmon_data:
+                                data[sensor_name].update(label_name)
 
                         if '_average' in file_:
-                            label_name, value = self.extract_data(sub_folder_path, file_)
-                            data[sensor_name][label_name] = value
+                            hwmon_data = self.extract_data(sub_folder_path, file_)
+                            for label_name in hwmon_data:
+                                data[sensor_name].update(label_name)
 
                     except Exception:
                         pass
@@ -111,7 +137,7 @@ class Hwmon():
 
             return data
 
-        def print_data(self, colors=False):
+        def print_data(self, colors=True):
             print_dict(self.data(), indent=0, colors=colors)
 
     class CPU():
